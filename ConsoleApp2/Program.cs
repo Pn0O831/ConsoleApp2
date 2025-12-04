7a2315af431e409421786dc617a9b2e028d6a4be
Pn0 <superumesyu@gmail.com>
2025-12-04 10:28:47 +0900

    15行目の内容について、修正しました。

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,7 +12,7 @@ Console.WriteLine($"{num1 / num2} 余り{num1 % num2}");
 //②
 int IntTest = 1;
 string StringTest = "01";
-string num3 = "1";
+string num3 = Convert.ToString(IntTest);
 Console.WriteLine($"String {num3}");
 int num4 = Convert.ToInt32(StringTest);
 Console.WriteLine(++num4);
